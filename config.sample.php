17111d25d5383c023e321365abb716a156be186d
josnw <66671217+josnw@users.noreply.github.com>
2020-06-11 13:24:24 +0200

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,8 @@
 <?php
 
-$ebfilename = "./docs/ZAVIS.DAT";
 $ebblz = "DUMMYBLZ4EB";
 $ebkonto = "DUMMYKONTO4EB";
 
-$basepath="http://yourintranet/converter/";
 $factopg="host='IPADDRESS SQL SERVER' port='5432' dbname='FACTO5-001' user='guest' password='XXXXXX'";
 
 $docpath = "./docs/";
