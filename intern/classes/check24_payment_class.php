d0d294d342cff58e2453a08eb16e160878f709f1
josnw <66671217+josnw@users.noreply.github.com>
2022-03-11 09:42:25 +0100

    add tax rate check24 charge info

--- intern/classes/check24_payment_class.php
+++ intern/classes/check24_payment_class.php
@@ -150,7 +150,7 @@ class check24Payment {
 						'CHARGE_AMOUNT' => str_replace(".",",",sprintf("%01.2f",$rowdata[$this->mapping['TRANSACTION_CHARGEAMOUNT']])),
 						'CHARGE_NDDT' => 'NONREF',
 						'CHARGE_TEXT00' => 'check24.DE',
-						'CHARGE_TEXT20' => 'check24.DE GEB.',
+							'CHARGE_TEXT20' => 'check24.DE GEB.'. sprintf("%d",$rowdata[$this->mapping['TAX_RATE']]),
 						'CHARGE_TEXT21' => $rowdata[$this->mapping['TRANSACTION_CODE']],
 						'CHARGE_TEXT22' => strtoupper($name),
 						'PAYMENT_STATE' =>  'S'
