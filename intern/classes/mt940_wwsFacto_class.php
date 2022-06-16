ecfe5003716bce1c1ebb3f23d4ec7349d0695660
josnw <66671217+josnw@users.noreply.github.com>
2022-06-16 07:38:21 +0200

    fix browsertimeout

--- intern/classes/mt940_wwsFacto_class.php
+++ intern/classes/mt940_wwsFacto_class.php
@@ -13,6 +13,8 @@ class MT940_wwsFacto {
 		
 		// connect to wws/erp fpr invoice details
 		$this->pg_pdo = new PDO($parameter['wwsserver'], $parameter['wwsuser'], $parameter['wwspass'], null);
+		//prevent Browser Timeout
+		print " ";
 	}
 	
 	public function getInvoiceData($ppid, $fromDate = '1999-12-31', $toDate = '2999-12-31', $fromCustomer = 0, $toCustomer = 999999) {
